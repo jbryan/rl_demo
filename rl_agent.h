a6b198bc72aa3eaa3ac73c0b5190909dcd0ca16d
Josh Bryan <josh.bryan@gmail.com>
2010-12-16 22:24:06 -0600

    added copyright and readmen

--- rl_agent.h
+++ rl_agent.h
@@ -1,3 +1,23 @@
+/*
+ *	Author: Josh Bryan <josh.bryan@gmail.com>
+ *	This file is copyright 2010 by Josh Bryan.
+ *
+ *  This file is part of Reinforcement Learning Demo.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  Foobar is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef RL_AGENT_H_
 #define RL_AGENT_H_
 
